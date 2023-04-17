fe43896728a1d887742523db4e9fd5cbc488e04e
Luna <luna.software.development@gmail.com>
2023-04-17 11:31:22 +0200

    arrays MD

---
+++ array inhalen/Arrays/Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] getallen = { 10 ,20 , 30, 40, 50, 60 };
+            int som = berekenen(getallen);
+            Console.WriteLine($"De som van de array is: {som}");
+            Console.WriteLine($"Is het correct? {som == 210}");
+        }
+
+        static int berekenen(int[] arr) { 
+            int som = 0;
+            foreach (int getal in arr)
+            {
+                som += getal;
+            }
+            return som;
+        }
+    }
+}
