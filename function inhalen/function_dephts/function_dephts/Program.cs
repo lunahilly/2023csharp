190b5fa50568d111c5ea620f9696355fb5a596da
Luna <luna.software.development@gmail.com>
2023-04-19 11:37:20 +0200

    functions inhalen

---
+++ function inhalen/function_dephts/function_dephts/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace function_dephts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            First("Hello");
+            Second("My age is: ", 18);
+            Third("I give this assignment an ", 8, true);
+        }
+
+        //string function
+        static void First(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        static void Second(string text , int number)
+        {
+            Console.WriteLine(text);
+            Console.WriteLine(number);
+        }
+
+        static void Third(string text , int number, bool maybe)
+        {
+            Console.WriteLine(text);
+            Console.WriteLine(number);
+            Console.WriteLine(maybe);
+        }
+    }
+}
